8eae23190a66c17a0d403ebee98d494d01c33889
Krzysztof Drabek <krzysztof.drabek@runbmefish.pl>
2018-05-29 15:11:52 +0200

    Export create router.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import startQueues, { stopQueues, pool } from './app';
 import { createDispatcher, createSubscriber } from './communication';
 import createRedisPool, { drainRedisPool } from './redis';
+import createRouter from './routing';
 
 export default {
   startQueues,
   stopQueues,
+  createRouter,
   pool,
   createDispatcher,
   createSubscriber,
